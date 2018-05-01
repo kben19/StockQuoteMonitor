e3146400a377e60798b0dd4677f69d02bef63f79
kben19 <kben19@student.monash.edu>
2018-05-01 20:47:44 +1000

    Controller java file

---
+++ src/Controller.java
@@ -0,0 +1,34 @@
+/**
+ * Created by benzali on 5/1/2018.
+ */
+class Controller implements java.awt.event.ActionListener {
+
+    //Joe: Controller has Model and View hardwired in
+    Model model;
+    View view;
+
+    Controller() {
+        System.out.println ("Controller()");
+    } //Controller()
+
+    //invoked when a button is pressed
+    public void actionPerformed(java.awt.event.ActionEvent e){
+
+        System.out.println("Controller: acting on Model");
+        model.addData(view.getTextField());
+        view.update();
+    } //actionPerformed()
+
+
+    public void addModel(Model m){
+        System.out.println("Controller: adding model");
+        this.model = m;
+    } //addModel()
+
+    public void addView(View v){
+        System.out.println("Controller: adding view");
+        this.view = v;
+    } //addView()
+
+
+} //Controller
