e41c9529835bb797561c55b3c80e5f8f33caaf12
kben19 <kben19@student.monash.edu>
2018-05-01 20:49:08 +1000

    Upload Main File

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Created by benzali on 5/1/2018.
+ */
+public class Main{
+
+    public static void main(String[] args){
+
+        SQService mainRun = new SQService();
+
+    } //main()
+
+} //Main
